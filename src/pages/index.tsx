f2037f1e847ee25e3a249c8337b6207def536926
Francisco Fetapi <franciscofetapi10@gmail.com>
2023-05-19 20:05:16 +0100

    fix: all label images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,19 +9,19 @@ const mockdata = [
     date: "August 18, 2022",
   },
   {
-    title: "Best forests to visit in North America",
+    title: "Melhores florestas para visitar no Sul da America.",
     image:
       "https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
     date: "August 27, 2022",
   },
   {
-    title: "Hawaii beaches review: better than you think",
+    title: "Prais do Hawaii melhores do que se pensava.",
     image:
       "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
     date: "September 9, 2022",
   },
   {
-    title: "Mountains at night: 12 best locations to enjoy the view",
+    title: "Montanhas a noite, que vista incrivel.",
     image:
       "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
     date: "September 12, 2022",
